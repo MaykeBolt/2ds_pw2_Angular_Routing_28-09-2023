a29a83487678252726a724fd14d00bcf87602b9d
MaykeBolt <maykesouza2805@gmail.com>
2023-10-02 22:41:33 -0300

    Criação de componentes (Ver 21:03)

--- src/app/components/widgets/useridentification/useridentification.component.ts
+++ src/app/components/widgets/useridentification/useridentification.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./useridentification.component.css']
 })
 export class UseridentificationComponent {
+  loggedIn: boolean = false;
+  username: string = '';
 
+  isUserLoggedIn(): boolean {
+    return this.loggedIn;
+  }
+
+  login() {
+    this.loggedIn = true;
+    this.username = 'John Doe';
+  }
+
+  logout() {
+    this.loggedIn = false;
+    this.username = '';
+  }
 }
